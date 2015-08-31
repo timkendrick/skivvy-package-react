c7dd4cd22b4a74d44a30619f46a71fe095a37fac
Tim Kendrick <timkendrick@gmail.com>
2015-08-31 22:52:04 +0100

    Add configuration defaults

--- index.js
+++ index.js
@@ -5,23 +5,19 @@ exports.tasks = {
 	'build': require('./lib/tasks/build'),
 	'serve': require('./lib/tasks/serve'),
 	'create-app': require('./lib/tasks/create-app'),
-	'create-component': require('./lib/tasks/create-component'),
-	'create-view': require('./lib/tasks/create-view')
+	'create-view': require('./lib/tasks/create-view'),
+	'create-component': require('./lib/tasks/create-component')
 };
 
 exports.defaults = {
 	source: {
-		index: null,
-		scripts: null,
-		styles: null,
-		assets: null,
-		tests: null
+		scripts: 'src/scripts',
+		styles: 'src/styles',
+		templates: 'src/templates',
+		assets: 'src/assets'
 	},
-	destination: {
-		root: null,
-		js: null,
-		css: null
-	},
+	tests: 'test',
+	destination: 'dist',
 	port: 3000,
 	debug: false
 };
